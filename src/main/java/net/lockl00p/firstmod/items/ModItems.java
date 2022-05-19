5a8b5d04014a69e16bcf5387e0a5830e950d4a59
Lockl00p <97256723+Lockl00p@users.noreply.github.com>
2022-05-19 09:13:03 -0500

    Created Mythril

--- src/main/java/net/lockl00p/firstmod/items/ModItems.java
+++ src/main/java/net/lockl00p/firstmod/items/ModItems.java
@@ -1,11 +1,14 @@
 package net.lockl00p.firstmod.items;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.lockl00p.firstmod.FirstMod;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
+    public static final Item MYTHRIL_INGOT = regItem("mythril_ingot", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     public static Item regItem(String name, Item item){
         return Registry.register(Registry.ITEM, new Identifier(FirstMod.mod_id, name), item);
     }
