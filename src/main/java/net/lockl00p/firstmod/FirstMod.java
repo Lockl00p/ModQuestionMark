6ba24482ef134ef62d34698f9f8edba8ab4d3153
Lockl00p <97256723+Lockl00p@users.noreply.github.com>
2022-05-18 20:18:45 -0500

    Added modid constant and item registration

--- src/main/java/net/lockl00p/firstmod/FirstMod.java
+++ src/main/java/net/lockl00p/firstmod/FirstMod.java
@@ -1,6 +1,7 @@
 package net.lockl00p.firstmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.lockl00p.firstmod.items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +16,6 @@ public class FirstMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.regModItems();
 	}
 }
--- src/main/java/net/lockl00p/firstmod/items/ModItems.java
+++ src/main/java/net/lockl00p/firstmod/items/ModItems.java
@@ -1,9 +1,15 @@
 package net.lockl00p.firstmod.items;
 
 import net.lockl00p.firstmod.FirstMod;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-    public static void regItems(){
+    public static Item regItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(FirstMod.mod_id, name), item);
+    }
+    public static void regModItems(){
         FirstMod.LOGGER.info(("Registering Mod Items for " + FirstMod.mod_id));
     }
 }
