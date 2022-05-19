997900139c8acb96f4e1bb41fc6c94f83d80c7f5
Lockl00p <97256723+Lockl00p@users.noreply.github.com>
2022-05-18 20:10:47 -0500

    Added modid constant and item registration

--- src/main/java/net/lockl00p/firstmod/items/ModItems.java
+++ src/main/java/net/lockl00p/firstmod/items/ModItems.java
@@ -1,7 +1,9 @@
 package net.lockl00p.firstmod.items;
 
+import net.lockl00p.firstmod.FirstMod;
+
 public class ModItems {
     public static void regItems(){
-
+        FirstMod.LOGGER.info(("Registering Mod Items for " + FirstMod.mod_id));
     }
 }
